8c0523d7c93ad51df445656c222a14c4aa037f6e
DianaFarhat <dsf04@mail.aub.edu>
2025-05-19 15:57:42 +0300

    Setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,36 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom"; 
-//import { store } from "./store";
-import { Provider } from "react-redux";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-// Define the router with App as a layout
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "", // ✅ this allows children below
-        element: <Home />,
-        children: [
-        ],
-      },
-     /*  { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> }, */
-
-    ],
-  },
-]);
-
-// Wrap the entire application inside <Provider>
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>  
-    <RouterProvider router={router} />
-  </Provider>
-);
-
-console.log('App mounted'); // Debugging check
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
